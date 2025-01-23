08db447bb88333f927bec6e9f7300593f4956893
James Yoo <james.yoo@kellogg.northwestern.edu>
2025-01-23 00:55:03 +0000

    In-class assignment

---
+++ school-5-insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO students(
+    first_name,
+    last_name,
+    email
+) VALUES (
+    "James",
+    "Yoo",
+    "jhyoo3636@gmail.com"
+);
---
+++ school-6-update.sql
@@ -0,0 +1,3 @@
+UPDATE students 
+SET email = "james.yoo@kellogg.northwestern.edu"
+WHERE id = 1;
